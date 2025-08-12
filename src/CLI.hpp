e1ae1454846cad661c3136449e30a125c120674e
P0ulpy <florian.aurousseau@protonmail.com>
2025-08-12 12:26:34 +0200

    Update usage to be more consistent

--- src/CLI.hpp
+++ src/CLI.hpp
@@ -8,18 +8,18 @@
 
 const char* OutputFormatsToCstr(CompilerOptions::OutputFormats tokenType);
 
-constexpr const char* ShortCLIUsageStr = "Usage : log-compiler InputFile [-o outputFile] [--format json|(markdown|md)|c] [--verbose] [--debug] [-v] [-h]";
+constexpr const char* ShortCLIUsageStr = "Usage : log-compiler InputFile [--output outputFile] [--format json|(markdown|md)|c] [--render] [--version] [--help] [--verbose] [--debug]";
 
-constexpr const char* CompleteCLIUsageStr = 
+constexpr const char* CompleteCLIUsageStr =
 R"(positional arguments:
   InputFile         Specifies the input log file to be compiled.
 
 optional arguments:
-  -o, --output      Specifies the output file path (optional, default=./a.{format}).
-  --format          Specifies the output format as `JSON`, `Markdown` or `C + RayGui` format : `[json|(markdown|md)|c]` (optional, default=json).
-  --verbose         Enables verbose mode for detailed output (optional).
-  --debug           Enables debug mode for debugging information (optional).
+  -o, --output      Specifies the output file path (default=./a.{format}).
+  -f, --format      Specifies the output format as `JSON`, `Markdown` or `C + RayGui` format : `[json|(markdown|md)|c]` (default=json).
   -v, --version     display compiler version, then exit.
+  --verbose         Enables verbose mode for detailed output.
+  -d, --debug       Enables debug mode for debugging information.
   -h, --help        display this message, then exit.
 )";
 
@@ -27,7 +27,7 @@ void StdOutInputFileInfo(const std::string_view& inputFilePath, const std::strin
 void StdOutTokens(const std::vector<Token>& tokens);
 void StdOutProgram(const ProgramRoot& program);
 
-struct ParsedCLIParameters 
+struct ParsedCLIParameters
 {
     std::string inputFilePath;
     std::string outputFilePath;
